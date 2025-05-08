271ac60c5718af9177b2965afb009c731e56c1c4
Mohamad Ibrahem <37690661+mohamadibrahem@users.noreply.github.com>
2025-05-08 06:25:08 +0300

    fix

--- electron/main.ts
+++ electron/main.ts
@@ -1,22 +1,48 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Tray, Menu } from 'electron';
+import path from 'path';
 
 let win: BrowserWindow | null;
+let tray: Tray | null = null;
 
 console.log('🚀 main.ts: Electron بدأ العمل');
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1500,
+    height: 900,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,  // استخدم هذه القيمة مع contextIsolation = true للأمان
+      contextIsolation: true
     },
   });
 
   win.loadURL('http://localhost:3000');
+
+  win.on('closed', () => {
+    win = null;
+  });
 }
 
 app.whenReady().then(() => {
+  try {
+    tray = new Tray(path.join(__dirname, '../../src/assets/img/logo_brandk_icon.ico'));
+
+    tray.setToolTip('BRANDK ONLINE');
+  
+    tray.setContextMenu(Menu.buildFromTemplate([
+      {
+        label: 'Open App',
+        click: () => createWindow()
+      },
+      {
+        label: 'Quit',
+        click: () => app.quit()
+      }
+    ]));
+  } catch (error) {
+    console.error('Error loading tray icon:', error);
+  }
+
   createWindow();
 
   app.on('activate', () => {
