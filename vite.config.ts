bc71d24868fcd12173814016c796dfd752f572db
Mohamad Ibrahem <37690661+mohamadibrahem@users.noreply.github.com>
2025-05-16 08:38:18 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   assetsInclude: ['**/*.ttf', '**/*.woff', '**/*.woff2'],
   plugins: [
     vue(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/assets/fonts/Tajawal-Regular.ttf',
-          dest: 'assets/fonts'
-        }
-      ]
-    })
 ],
   resolve: {
     alias: { '@': resolve(__dirname, 'src') }
